717fc496db11fee8e836e2cfdcba00fd2df73f9d
yes <yes@yes.note>
2019-01-13 22:55:31 +0600

    Hydrologist should see stations only which are observed by him

--- hydrology/models.py
+++ hydrology/models.py
@@ -1,12 +1,11 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 from django.contrib.auth.models import User
+
+class Hydrologist(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+
+    class Meta:
+        db_table = 'hydrologists'
 
 class Region(models.Model):
     code = models.CharField(primary_key = True, max_length = 31)
@@ -24,39 +23,39 @@ class Hydropost(models.Model):
     lat = models.DecimalField(max_digits = 5, decimal_places=2)
     lon = models.DecimalField(max_digits = 5, decimal_places=2)
     region = models.ForeignKey(Region, on_delete = models.DO_NOTHING)
+    hydrologists = models.ManyToManyField(
+                    Hydrologist,
+                    through = 'Observation',
+                    through_fields = ('hydropost', 'hydrologist'),
+                    )
 
     def __str__(self):
         return self.name
     class Meta:
         db_table = 'hydroposts'
 
-class Hydrologist(models.Model):
-    user = models.OneToOneField(User, on_delete = models.CASCADE)
-    region = models.ForeignKey(Region, on_delete = models.DO_NOTHING)
+#Observation contains hydrologist and hydropost
+#This model contain list, which hydroposts can be observed
+#by which hydrologist
+class Observation(models.Model):
+    hydropost = models.ForeignKey(Hydropost, on_delete = models.CASCADE)
+    hydrologist = models.ForeignKey(Hydrologist, on_delete = models.CASCADE)
 
     class Meta:
-        db_table = 'hydrologists'
+        db_table = 'observations'
 
 class Discharge(models.Model):
     discharge = models.DecimalField(max_digits = 5, decimal_places = 2)
     observationDate = models.DateTimeField()
-    hydrologist = models.ForeignKey(Hydrologist, models.DO_NOTHING)
-    hydropost = models.ForeignKey(Hydropost, on_delete = models.DO_NOTHING)
+    observation = models.ForeignKey(Observation, on_delete = models.DO_NOTHING)
 
     class Meta:
         db_table = 'discharge'
-        unique_together = (('hydropost', 'observationDate'),)
-
 
 class Level(models.Model):
     level = models.DecimalField(max_digits = 5, decimal_places = 2)
     observationDate = models.DateTimeField()
-    hydrologist = models.ForeignKey(Hydrologist, on_delete = models.DO_NOTHING)
-    hydropost = models.ForeignKey(Hydropost, on_delete = models.DO_NOTHING)
+    observation = models.ForeignKey(Observation, on_delete = models.DO_NOTHING)
 
     class Meta:
         db_table = 'level'
-        unique_together = (('hydropost', 'observationDate'),)
-
-
-
--- hydrology/tests.py
+++ hydrology/tests.py
@@ -1,13 +1,58 @@
 from django.test import TestCase
 from django.test import Client
-from django.urls import resolve
+from django.urls import resolve, reverse
 from django.http import HttpRequest
 from django.contrib.auth.models import User
 
 from .views import home
+
+from .models import Hydropost, Hydrologist, Region, Observation
 
 class LoginPageTest(TestCase):
    
     def test_login_page_resolves_as_login_view(self):
         login_url = resolve('/login/')
         self.assertEqual(login_url.url_name, 'login')
+    
+    def test_not_logged_home_page_redirects_to_correct_url(self):
+        response = self.client.get('/')
+        self.assertRedirects(response, '/login/?next=/')
+
+class LoggedInTestCase(TestCase):
+    fixtures = ['hydrology/fixtures/fixtures.json']
+    
+    def setUp(self):
+        new_user = User.objects.create_user(username='username', password='password')
+        self.client.login(username='username', password='password')
+        new_hydrologist = Hydrologist.objects.create(user = new_user)  
+        #Р. Силеты – Новомарковка
+        firstHydropost = Hydropost.objects.get(code = 11242)
+        #Р.Есиль – с. Державинск
+        secondHydropost = Hydropost.objects.get(code = 11402)
+        Observation.objects.create(hydropost = firstHydropost,
+                hydrologist = new_hydrologist)
+        Observation.objects.create(hydropost = secondHydropost,
+                hydrologist = new_hydrologist)
+
+
+class HomePageTest(LoggedInTestCase):
+
+    def test_root_url_resolves_as_home_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'hydrology/home.html')
+
+
+
+class HydropostListModelTest(LoggedInTestCase):
+
+    def test_get_hydrologist_see_his_region_hydroposts(self):
+        user = User.objects.get(username = 'username') 
+        hydrologist = Hydrologist.objects.get(user = user)
+        hydrologist_hydroposts = hydrologist.hydropost_set.all().values_list('name' , flat = True)
+        his_hydroposts = ['Р. Силеты – Новомарковка', 'Р.Есиль – с. Державинск']        
+        self.assertListEqual(list(hydrologist_hydroposts), list(his_hydroposts))
