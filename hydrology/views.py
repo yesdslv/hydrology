e8bcef01e7c71c9afc5053e8cfed2c5818844d25
yes <yes@yes.note>
2019-01-18 12:31:42 +0600

    Start to add new view

--- hydrology/views.py
+++ hydrology/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 from .models import Hydrologist, Hydropost
+from .forms import GP1Form, OGP2Form
+
 
 @login_required(login_url = '/login/')
 def home(request):
@@ -15,5 +17,21 @@ def home(request):
 @login_required(login_url = '/login/')
 def record(request):
     hydropost = get_object_or_404(Hydropost, nameEn = request.POST.get('hydropost', False))
-    context = { 'hydropost' : hydropost ,}
+    if request.method == 'POST':
+        form = GP1Form()
+        if form.is_valid():
+            return redirect('/')
+    else:
+        form = GP1Form()
+    context = { 'hydropost' : hydropost ,
+                'form': form, }
     return render(request, 'hydrology/record.html', context)
+
+#@login_required(login_url = '/login/')
+#def search_hydropost_type(request):
+#    if request.method == 'GET':
+#        if 'hydropost' in request.GET.keys():
+#                message = 'You submitted: %r' % request.GET['q']
+#            else:
+#                message = 'You submitted nothing!'
+#    return HttpResponse(message)
