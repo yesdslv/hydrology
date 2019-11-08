a862944d6e17d64c07f5e507b6afa3588ca541f8
Yesset <y.dyuisaliyev@kazdream.kz>
2019-11-08 16:28:54 +0600

    Removed meteorology app

--- src/meteorology/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MeteorologyConfig(AppConfig):
-    name = 'meteorology'
--- src/meteorology/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Meteopost(models.Model):
-    code = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=255)
-
-class MeteopostPhoto(models.Model):
